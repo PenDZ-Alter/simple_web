aef272cf27efe5e6ca23a73a3b3ab7c2931ac5f7
PenDZ-Alter <abxpendz45@gmail.com>
2024-06-17 13:41:20 +0700

    Update Code

--- blogs/blog.php
+++ blogs/blog.php
@@ -19,7 +19,7 @@
 <body>
   <?php include "../components/header.php"; ?>
   <?php
-    $query = "SELECT a.*, c.*, d.* FROM user a JOIN artikel c JOIN kontributor d ON a.id_user = d.id_user AND c.id_artikel = d.id_artikel WHERE d.id_kontributor='$id'";
+    $query = "SELECT a.*, c.*, d.*, k.* FROM user a JOIN artikel c JOIN kategori k JOIN kontributor d ON a.id_user = d.id_user AND c.id_artikel = d.id_artikel AND k.id_kategori = d.id_kategori WHERE d.id_kontributor='$id'";
     $result = $conn -> query($query);
     $res = $result -> fetch_array();
   ?>
@@ -28,7 +28,7 @@
   <div class="container mx-auto py-8 px-4 lg:px-8">
     <div class="bg-gray-800 rounded-lg shadow-md p-6">
       <h1 class="text-3xl font-bold text-white mb-4"><?php echo $res['judul']; ?></h1>
-      <p class="text-white mb-6">Created By <span class="font-semibold text-gray-800"><?php echo $res['nama']; ?></span></p>
+      <p class="text-white mb-6">Created By <span class="font-semibold text-gray-800"><?php echo $res['nama']; ?></span> · <?php echo $res['nama_kategori'] ?></p>
       <article class="prose max-w-none text-white">
         <?php echo nl2br($res['isi']); ?>
       </article>
